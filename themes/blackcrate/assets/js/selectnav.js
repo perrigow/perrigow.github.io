b2cc6deace68d9795351c9b7c01a486cafc134aa
Wayne Perrigo <83511562+perrigow@users.noreply.github.com>
2023-08-12 22:08:12 -0400

    new layout and theme

--- themes/blackcrate/assets/js/selectnav.js
+++
@@ -1,9 +0,0 @@
-/*!
- * SelectNav.js
- * Navigation for small screens
- */
-
-document.getElementById('selectnav-main')
-        .addEventListener('change', function(event) {
-            window.location = this.value;
-        });
